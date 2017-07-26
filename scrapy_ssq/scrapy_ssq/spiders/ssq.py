6e2dca1dedfc286e6722b5aad01ae4f2625b008f
WesleyHu <Wesley.hu@libratone.com.cn>
2017-07-26 18:29:39 +0800

    modify ssq.py

--- scrapy_ssq/scrapy_ssq/spiders/ssq.py
+++ scrapy_ssq/scrapy_ssq/spiders/ssq.py
@@ -4,55 +4,53 @@ from scrapy.http import Request
 from scrapy_ssq.items import ScrapySsqItem
 import requests
 import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 class myspider(scrapy.Spider):
     name = 'ssq'
-    allowed_domains =['cjcp.com.cn']
-
+    allowed_domains =['kaijiang.zhcw.com']
 
 
     def start_requests(self):
-        theme_url = 'http://http://shuangseqiu.cjcp.com.cn/kaijiang/'#爬虫开始的页面
+        theme_url = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_1.html'#爬虫开始的页面
         yield Request(theme_url,self.parse)#对每个链接调用parse函数
     def parse(self, response):
-        page = BeautifulSoup(response.text, 'lxml').find('div', class_='n_dm_page')#找出最大的页数，然后遍历
-        for p in page:
-            if p:
-                max_page = re.findall('span>/(\d+)页次', str(p))
-                for max in max_page:
-                    if max:
-                        print(max)
-                        bashurl = str(html.url)[:-1]
-                        print(bashurl)
-                        for num in range(1,int(max)+1):
-                            url = bashurl+str(num)+'/'
-                            print(url)
-                            yield Request(url,callback=self.get_message)
+        page = BeautifulSoup(response.text, 'lxml').find('p', class_='pg')#找出最大的页数，然后遍历
+        max_page = re.findall('<strong>(\d+)<', str(page))[0]
+        print("max_page = " + max_page)
+        bashurl = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_'
+        for num in range(1,int(max_page)+1):
+            url = bashurl+str(num)+'.html'
+            print(url)
+            yield Request(url,callback=self.get_message)
     def get_message(self,response):
         item = ScrapySsqItem()
         content = BeautifulSoup(response.text, 'lxml')
-        tables = content.findAll('table')
-        for table in tables:
-            if re.search("kjjg_table", str(table)):
-                rows = table.findAll('tr')
-                for row in rows:
-                    if re.search("双色球开奖结果第", str(row)):
-                        columns = row.findAll('td')
-                        # find rid
-                        rid = re.findall(r'(\d+)', str(columns[0]))[0]
-                        item['rid'] = rid
-                        # find date
-                        date = str(columns[1])[4:14]
-                        item['date'] = date
-                        # find red and blue number
-                        rbs = re.findall('class="hm_bg">(\d+)<', str(columns[2]))
-                        item['rb1'] = rbs[0]
-                        item['rb2'] = rbs[1]
-                        item['rb3'] = rbs[2]
-                        item['rb4'] = rbs[3]
-                        item['rb5'] = rbs[4]
-                        item['rb6'] = rbs[5]
-                        bb = re.findall('class="lqhm_bg">(\d+)<', str(columns[2]))[0]
-                        item['bb'] = bb   
-                        print("rid=" + rid + ", date=" + date + ", rb1=" + rbs[0] + ", rb2=" + rbs[1] + ", rb3=" + rbs[2] + ", rb4=" + rbs[3] + ", rb5=" + rbs[4] + ", rb6=" + rbs[5] + ", bb=" + bb)
-                        yield item
+        rows = content.findAll('tr')
+        for row in rows:
+        if re.search("em class", str(row)):
+            columns = row.findAll('td')
+            rid = re.findall(r'(\d+)', str(columns[1]))[0]
+            date = str(columns[0])[19:29]
+            balls = re.findall(r'(\d+)', str(columns[2]))
+            rb1 = balls[1]
+            rb2 = balls[2]
+            rb3 = balls[3]
+            rb4 = balls[4]
+            rb5 = balls[5]
+            rb6 = balls[6]
+            bb = balls[7]
+            print("rid=" + rid + ", date=" + date + ", rb1=" + rb1 + ", rb2=" + rb2 + ", rb3=" + rb3 + ", rb4=" + rb4 + ", rb5=" + rb5 + ", rb6=" + rb6 + ", bb=" + bb)
+            item['rid'] = rid
+            item['date'] = date
+            item['rb1'] = rb1
+            item['rb2'] = rb2
+            item['rb3'] = rb3
+            item['rb4'] = rb4
+            item['rb5'] = rb5
+            item['rb6'] = rb6
+            item['bb'] = bb
+            yield item
--- scrapy_ssq/scrapy_ssq/spiders/temp.py
+++ scrapy_ssq/scrapy_ssq/spiders/temp.py
@@ -4,50 +4,37 @@ import scrapy
 from bs4 import BeautifulSoup
 import requests
 import re
-'''
-theme_url='http://shuangseqiu.cjcp.com.cn/kaijiang/'#爬虫开始的页面
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+#theme_url='http://shuangseqiu.cjcp.com.cn/kaijiang/'#爬虫开始的页面
+theme_url='http://kaijiang.zhcw.com/zhcw/html/ssq/list_1.html'
 html = requests.get(theme_url)
 print(html.status_code)
-page=BeautifulSoup(html.text, 'lxml').find('div', class_='n_dm_page')#找出每个区域最大的页数，然后遍历
-print (page)
-for p in page:
-	if p:
-		max_page = re.findall('span>/(\d+)页次', str(p))
-		for max in max_page:
-			if max:
-				print(max)
-				bashurl = str(html.url)[:-1]
-				print(bashurl)
-				for num in range(1,int(max)+1):
-			              	url = bashurl+str(num)+'/'
-			              	print(url)
-			              	response=requests.get(url)
-			              	print(response.status_code)
-			              	content=BeautifulSoup(response.text, 'lxml')
-'''
-theme_url='http://shuangseqiu.cjcp.com.cn/kaijiang/'#爬虫开始的页面
-html = requests.get(theme_url)
-content=BeautifulSoup(html.text, 'lxml')
-tables = content.findAll('table')#找到所在的标签里
-for table in tables:
-    if re.search("kjjg_table", str(table)):
-        rows = table.findAll('tr')
-        for row in rows:
-            if re.search("双色球开奖结果第", str(row)):
-                print("------------ result -----------")
-                columns = row.findAll('td')
+page=BeautifulSoup(html.text, 'lxml').find('p', class_='pg')#找出每个区域最大的页数，然后遍历
+max_page = re.findall('<strong>(\d+)<', str(page))[0]
+print(max_page)
+#bashurl = 'http://www.cjcp.com.cn/ajax_kj.php?jsoncallback=jsonp1500982504171&ssq_type=page&pagenum='
+bashurl = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_'
+for num in range(1,int(max_page)+1):
+    url = bashurl+str(num)+'.html'
+    print(url)
+    response=requests.get(url)
+    content=BeautifulSoup(response.text, 'lxml')
+    rows = content.findAll('tr')
+    for row in rows:
+        if re.search("em class", str(row)):
+            columns = row.findAll('td')
+            rid = re.findall(r'(\d+)', str(columns[1]))[0]
+            date = str(columns[0])[19:29]
+            balls = re.findall(r'(\d+)', str(columns[2]))
+            rb1 = balls[1]
+            rb2 = balls[2]
+            rb3 = balls[3]
+            rb4 = balls[4]
+            rb5 = balls[5]
+            rb6 = balls[6]
+            bb = balls[7]
+            print("rid=" + rid + ", date=" + date + ", rb1=" + rb1 + ", rb2=" + rb2 + ", rb3=" + rb3 + ", rb4=" + rb4 + ", rb5=" + rb5 + ", rb6=" + rb6 + ", bb=" + bb)
 
-                rid = re.findall(r'(\d+)', str(columns[0]))[0]
-
-                date = str(columns[1])[4:14]
-
-                rbs = re.findall('class="hm_bg">(\d+)<', str(columns[2]))
-                rb1 = rbs[0]
-                rb2 = rbs[1]
-                rb3 = rbs[2]
-                rb4 = rbs[3]
-                rb5 = rbs[4]
-                rb6 = rbs[5]
-                bb = re.findall('class="lqhm_bg">(\d+)<', str(columns[2]))[0]
-                print("rid=" + rid + ", date=" + date + ", rb1=" + rbs[0] + ", rb2=" + rbs[1] + ", rb3=" + rbs[2] + ", rb4=" + rbs[3] + ", rb5=" + rbs[4] + ", rb6=" + rbs[5] + ", bb=" + bb)
- 
+                                                              
